bed2c6f9dd19db3f22ad345a5f0939d5fb20c3ec
Admin <voldamarmc@gmail.com>
2025-08-18 18:35:11 +0200

    Version 1.1 Functions: Checks if server is running. Launches server if not running. Fixes: Removed spam

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import win32gui
 import win32con
 import win32api
 import os
+import socket
+
+def server_check(host="localhost", port=25565):
+    # Ping server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host, port))
+    except (socket.timeout, ConnectionRefusedError):
+        return False
+    else:
+        sock.close()
+        return True
 
 def get_cmd(window_title, text):
     # Find CMD window or launch server
@@ -13,16 +25,17 @@ def get_cmd(window_title, text):
         os.startfile("Run.bat")
         return
 
-    # Typing
-    for char in text:
-        win32api.SendMessage(hwnd, win32con.WM_CHAR, ord(char), 0)
+    time.sleep(6) # Checking timer
+    if server_check():
+        return
+    else:
+        # Press enter
+        win32api.SendMessage(hwnd, win32con.WM_CHAR, 13, 0)
 
-    # Entering
-    win32api.SendMessage(hwnd, win32con.WM_CHAR, 13, 0)
-
-
+#Dummy loop and flavour testing text
 Flag = 0
 
+
 while Flag == 0:
-    time.sleep(20)
+    time.sleep(5) # Safety timer
     get_cmd("C:\\Windows\\system32\\cmd.exe", "/say Testing")
